09b04770fb1c4dd3525aac639f29ac8e277482b0
Brian Robert Belal <belal.ibrahim@proton.me>
2026-06-15 17:20:09 +0200

    clean up install script

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo apt install  pandoc
-sudo apt install texlive-latex-base texlive-fonts-recommended texlive-latex-extra
-sudo apt install fonts-inter
-sudo apt install texlive-xetex
+sudo apt install pandoc \
+  texlive-latex-base texlive-fonts-recommended texlive-latex-extra texlive-xetex \
+  fonts-inter
